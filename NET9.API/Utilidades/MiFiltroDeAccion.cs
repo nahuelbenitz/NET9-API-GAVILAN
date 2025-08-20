10da9d1b8a5d1080d48474236d56be07ffe65f3e
nahuelbenitz <bnahuel83@gmail.com>
2025-08-20 20:00:52 -0300

    plantilla de filtros

---
+++ NET9.API/Utilidades/MiFiltroDeAccion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NET9.API.Utilidades
+{
+    public class MiFiltroDeAccion : IActionFilter
+    {
+        private readonly ILogger<MiFiltroDeAccion> _logger;
+
+        public MiFiltroDeAccion(ILogger<MiFiltroDeAccion> logger)
+        {
+            _logger = logger;
+        }
+
+        //Se ejecuta antes de que se ejecute la acción del controlador
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        //se ejecuta después de que se ha ejecutado la acción del controlador
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
